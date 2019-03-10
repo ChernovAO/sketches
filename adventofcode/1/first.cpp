32296c9ee6a534214350be3184856cc941cd3076
ChernovAO <chernov.aleksandre@gmail.com>
2019-03-10 15:16:02 +0300

    advent of code first. step 2

--- adventofcode/1/first.cpp
+++ adventofcode/1/first.cpp
@@ -1,4 +1,17 @@
+// --- Day 1: Chronal Calibration ---
+#include <filesystem>
+#include <iostream>
+#include <iomanip>
+
 int main(int argc, char** argv)
 {
+    if(argc != 2)
+    {
+        std::cout << "Mandatory parameter missed. \r\n"
+                  << "Usage: " << argv[0] << " <data_source>"
+                  << std::endl;
+        return 1;
+    }
+
     return 0;
 }
