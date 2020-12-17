386bd925b0fbc74b9be06488580ddb3a75f276d6
ChernovAO <chernov.aleksandre@gmail.com>
2020-12-17 19:42:12 +0300

    Ome step close to solvitask from leetcode

--- leetcode/cpp/zig_zag_conv/src/zig_zag_conv.cpp
+++ leetcode/cpp/zig_zag_conv/src/zig_zag_conv.cpp
@@ -4,6 +4,10 @@ namespace leetcode
 {
     std::string convert(std::string s, int numRows)
     {
+        const auto lenght = s.length();
+        const auto columnsInCycle = numRows - 1;
+        const auto charsInCycle = 2*numRows - 2;
+        const int full
         return {};
     }
 } // namespace leetcode
