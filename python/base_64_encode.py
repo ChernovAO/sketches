d4d8619859ff0083a4085e9c02bb09653cdf03fc
ChernovAO <chernov.aleksandre@gmail.com>
2020-12-17 19:44:36 +0300

    Initial commit for base 64 encode/decode python tool was added.

---
+++ python/base_64_encode.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Base 64 encode/decode util')
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
